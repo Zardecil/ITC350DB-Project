b06089a7933fc3d921fd6a272d57ddfc20e34b47
Zardecil <30330122+Zardecil@users.noreply.github.com>
2024-03-02 23:58:49 -0700

    Update dbdelete.sql

--- dbdelete.sql
+++ dbdelete.sql
@@ -1,10 +1,10 @@
 delete from "CLASS" where ClassName='rogue';
 delete from "RACE" where RaceID=2;
 delete from "USERS" where Username='burrito';
-delete from "BACKGROUND" where 1=1;
-delete from "ACTIONS" where 1=1;
-delete from "BACKGROUND_Predefined_Items" where 1=1;
-delete from "CHARACTERS" where 1=1;
+delete from "BACKGROUND" where BackgroundName='hermit';
+delete from "ACTIONS" where ActionDate='Eat A Taco';
+delete from "BACKGROUND_Predefined_Items" where Predefined_Items='Tacos';
+delete from "CHARACTERS" where Fname='Burrito';
 delete from "SKILL" where 1=1;
 delete from "ABILITY_SCORE" where 1=1;
 delete from "have" where 1=1;
